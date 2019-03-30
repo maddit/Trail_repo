27e6b141520b49c4920e12263c67b81690b0d7f3
Mohit Solanki <49089000+maddit@users.noreply.github.com>
2019-03-30 16:54:44 +0530

    Add files via upload

---
+++ Boot-Xampp.sh
@@ -0,0 +1,14 @@
+touch /etc/init.d/lampp
+chmod +x /etc/init.d/lampp
+echo "#!/bin/bash
+### BEGIN INIT INFO
+# Provides: lampp
+# Required-Start:    $local_fs $syslog $remote_fs dbus
+# Required-Stop:     $local_fs $syslog $remote_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start lampp
+### END INIT INFO
+/opt/lampp/lampp start" >> /etc/init.d/lampp
+
+update-rc.d lampp defaults
